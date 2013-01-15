ffe05ff3a86a2d595df32682929c808d0ca9c664
dobabeswe <chololoy@gmail.com>
2013-01-15 13:37:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CafeHobo2::Application.config.secret_token = '52854c69b7496aa550db16f2060753a0e5d6277167329cfd0d133a815d08ac09f1b1500b11e721bd1a9001958c8a1133c6f45400d93dffb2fdc1d425a15e6f8b'
