3690d8b3f2c668f2f2150e5f5ace274d4fb81a1b
dobabeswe <chololoy@gmail.com>
2013-02-01 06:21:21 -0600

    Add authorization to Portfolio_Sites controller

--- app/controllers/portfolio_sites_controller.rb
+++ app/controllers/portfolio_sites_controller.rb
@@ -1,22 +1,28 @@
 class PortfolioSitesController < ApplicationController
+  before_filter :authenticate_user!
   
   def index
+    authorize! :index, @portfolio_site, message: 'Not authorized as an administrator.'
   	@portfolio_sites = PortfolioSite.all
   end
 
   def show
+    authorize! :show, @portfolio_site, message: 'Not authorized as an administrator.'
   	@portfolio_site = PortfolioSite.find(params[:id])
   end
 
   def new
+    authorize! :new, @portfolio_site, message: 'Not authorized as an administrator.'
   	@portfolio_site = PortfolioSite.new
   end
 
   def edit
+    authorize! :edit, @portfolio_site, message: 'Not authorized as an administrator.'
   	@portfolio_site = PortfolioSite.find(params[:id])
   end
 
   def create 
+    authorize! :create, @portfolio_site, message: 'Not authorized as an administrator.'
   	@portfolio_site = PortfolioSite.new(params[:portfolio_site])
   	if @portfolio_site.save
   		flash[:success] = "Portfolio site was successfully created."
@@ -27,6 +33,7 @@ class PortfolioSitesController < ApplicationController
   end
 
   def update
+    authorize! :update, @portfolio_site, message: 'Not authorized as an administrator.'
   	@portfolio_site = PortfolioSite.find(params[:id])
   	if @portfolio_site.update_attributes(params[:portfolio_site])
   		flash[:success] = "Portfolio site updated."
@@ -37,6 +44,7 @@ class PortfolioSitesController < ApplicationController
   end
 
   def destroy
+    authorize! :destroy, @portfolio_site, message: 'Not authorized as an administrator.'
   	PortfolioSite.find(params[:id]).destroy
   	flash[:success] = "Portfolio site destroyed."
   	redirect_to portfolio_sites_url
