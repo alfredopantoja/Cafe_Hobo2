8ebec781bfb1390beb9a02f5105b0ba5d9684cc2
dobabeswe <chololoy@gmail.com>
2013-02-01 09:23:46 -0600

    Add default scope sort by position to portfolio sites and codeschool courses

--- app/models/codeschool_course.rb
+++ app/models/codeschool_course.rb
@@ -1,3 +1,5 @@
 class CodeschoolCourse < ActiveRecord::Base
   attr_accessible :description, :image_url, :name, :position
+
+  default_scope order: 'codeschool_courses.position ASC'
 end
--- app/models/portfolio_site.rb
+++ app/models/portfolio_site.rb
@@ -1,3 +1,5 @@
 class PortfolioSite < ActiveRecord::Base
   attr_accessible :description, :name, :position, :screenshot_url, :site_url
+
+  default_scope order: 'portfolio_sites.position ASC'
 end
